7a21fb0b105add5df810420dab66b4a5887f52e2
Michael Nagy <michaelnt8@gmail.com>
2017-06-10 17:03:29 -0300

    todoinput basic methods

--- src/components/TodoInput/TodoInput.js
+++ src/components/TodoInput/TodoInput.js
@@ -2,15 +2,39 @@ import React, { Component } from 'react';
 import { Form, Input, Segment } from 'semantic-ui-react'
 import './TodoInput.scss'
 
-const TodoInput = () => (
-  <Segment inverted className='todo-input'>
-    <Form.Group inline>
-      <Form.Field>
-        <Input fluid={true} size='massive' type='text' placeholder='Insert your task and hit Enter'/>
-      </Form.Field>
-    </Form.Group>
+class TodoInput extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState({value: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    console.log('Enviou');
     
-  </Segment>
-)
+
+    e.preventDefault();
+  }  
+
+
+  render () {
+    return (
+      <Segment inverted className='todo-input'>
+        <Form onSubmit={this.handleSubmit}>
+            <Form.Field>
+              <Input fluid={true} size='massive' onChange={ this.handleInputChange } placeholder='Insert your task and hit Enter'/>
+            </Form.Field>
+        </Form>
+        
+        
+      </Segment>
+    )
+  }
+}
 
 export default TodoInput
